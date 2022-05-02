986aa2b2c2b62418fcacab04b367cceb1084d219
Andy Mihalik <andymihalik@gmail.com>
2022-05-02 18:52:03 -0500

    easy sort kata

---
+++ SortNumbers/SortNumbers/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNumbers
+{
+    internal class Kata
+    {
+        public static int[] SortNumbers(int[] nums)
+        {
+            if (nums == null)
+                return new int[0];
+            return nums.OrderBy(n => n).ToArray();
+            
+        }
+    }
+}
---
+++ SortNumbers/SortNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SortNumbers;
+
+Console.WriteLine("Hello, World!");
+
+
+var unsortedNums = new int[] { 1, 3, 2, 4, 6, 5 };
+
+var kata = new Kata();
+Console.Write("Sorting: ");
+Array.ForEach(unsortedNums, Console.Write);
+Console.WriteLine();
+var sortedNums = Kata.SortNumbers(unsortedNums);
+Console.WriteLine("Sorted: ");
+Array.ForEach(sortedNums, Console.Write);
+Console.WriteLine();
+
